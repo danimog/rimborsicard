4b3b527a2250c72f3355c1684b8b5722a13150fa
danimog <daniele.moggia@gmail.com>
2021-09-27 15:32:52 +0200

    add login

---
+++ pages/login.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="container">
+        <h3 class="has-text-centered mt-5 mb-5">Accedi al tuo account</h3>
+
+        
+        <form :class="this.formErrore ? 'formErrore' : false">
+            <div class="field is-horizontal">
+                <div class="field-body">
+                    <div class="field">
+                        <label class="label">Email</label>
+                        <p class="control is-expanded has-icons-left">
+                        <input class="input" type="email" placeholder="email" v-model="email" required @invalid="invalidateForm">
+                        <b-icon
+                            icon="email"
+                        />&nbsp; 
+                        </p>
+                    </div>
+                    <div class="field">
+                        <label class="label">Password</label>
+                        <p class="control is-expanded has-icons-left">
+                        <input class="input" type="password" placeholder="password" v-model="password" required @invalid="invalidateForm">
+                        <b-icon
+                            icon="email"
+                        />&nbsp; 
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="control has-text-centered">
+                <input class="button is-primary" type="submit" value="Login" @click="">
+            </div>
+
+        </form>
+        <div class="error text-danger text-center" v-if="error">{{error.message}}</div>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+
+    export default {
+        data(){
+            return{
+                email: '',
+                password: '',
+                error: ''
+            }
+        },
+        methods: {
+            pressed(){
+                firebase
+                    .auth()
+                    .signInWithEmailAndPassword(this.email, this.password)
+                    .then(data => {
+                        
+                        this.$router.push({ path: '/leggi_rimborsi' })
+                    })
+                    .catch(error => {
+                        this.error = error
+                    })
+                
+            }
+        },
+        
+    }
+</script>
+
+<style scoped>
+.my-container{
+    max-width: 80%;
+    margin: 2rem auto;
+}
+.my-form{
+    margin: 10px auto;
+    max-width: 480px;
+    text-align: center;
+}
+</style>
